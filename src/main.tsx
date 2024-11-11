c2581277c3127fdc1669ad20283de2ddb168059c
Shreya Singhal <singhalshreya102@gmail.com>
2024-11-11 14:04:19 +0530

    initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient=new QueryClient()
+// react query provider
 
 createRoot(document.getElementById('root')!).render(
 
